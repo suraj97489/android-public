f1ea857c99333021f6af53bfa8c790f147807fb8
Suraj Atigre <suraj97489@gmail.com>
2022-02-02 13:22:36 +0530

    app working fine in expogo and on web

--- firebaseAndroid.js
+++ firebaseAndroid.js
@@ -5,13 +5,20 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  // apiKey: process.env.REACT_APP_API_KEY,
+  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_APP_ID,
+  // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyCDABZeTU2k7bzLIk14gDJkIxWLatN_d-8",
+  authDomain: "salonkatta-bff6d.firebaseapp.com",
+  projectId: "salonkatta-bff6d",
+  storageBucket: "salonkatta-bff6d.appspot.com",
+  messagingSenderId: "471341648576",
+  appId: "1:471341648576:web:2910cbda481ee746fdfb25",
+  measurementId: "G-5BR8J6NF36",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
