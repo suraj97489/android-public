4b039cc055fc7131d26168565a42cd8b04a6ab5a
Suraj Atigre <suraj97489@gmail.com>
2022-02-03 21:44:10 +0530

    added extra charges functionality for Report

--- components/DoneModal.js
+++ components/DoneModal.js
@@ -1,11 +1,12 @@
 import { Modal, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateDoneModal } from "../features/androidSlice";
 import { Button } from "react-native-elements/dist/buttons/Button";
 import colors from "../theme/colors";
 import { doc, runTransaction } from "firebase/firestore";
 import { db } from "../firebaseAndroid";
+import { Input } from "react-native-elements";
 
 const DoneModal = () => {
   const dispatch = useDispatch();
@@ -13,8 +14,19 @@ const DoneModal = () => {
   const doneModal = useSelector((state) => state.android.doneModal);
   const provider = useSelector((state) => state.android.doneProvider);
   const customer = useSelector((state) => state.android.doneCustomer);
+  const [extra, setExtra] = useState("");
+
+  let serviceWithCharges = customer?.service.map((eachServiceName) => {
+    return salon?.services.find((service) => service.name === eachServiceName);
+  });
+
+  let customerPaid = serviceWithCharges?.reduce((accumulte, service) => {
+    return accumulte + Number(service.charges);
+  }, 0);
+
   function closeDoneModal() {
     dispatch(updateDoneModal(false));
+    setExtra("");
   }
 
   async function done() {
@@ -47,15 +59,6 @@ const DoneModal = () => {
 
         let date = new Date().toDateString();
         let time = new Date().toLocaleTimeString();
-        let serviceWithCharges = customer.service.map((eachServiceName) => {
-          return salon?.services.find(
-            (service) => service.name === eachServiceName
-          );
-        });
-
-        let customerPaid = serviceWithCharges.reduce((accumulte, service) => {
-          return accumulte + Number(service.charges);
-        }, 0);
 
         let report = {
           custName: customer.name,
@@ -67,6 +70,7 @@ const DoneModal = () => {
           providerId: provider.id,
           customerPaid: customerPaid,
           addedBy: customer.addedBy,
+          extra: extra === "" ? 0 : Number(extra),
         };
 
         let salonReportUpdatedArray = [report, ...salon.salonReport];
@@ -76,6 +80,7 @@ const DoneModal = () => {
           salonReport: salonReportUpdatedArray,
         });
       });
+      setExtra("");
     } catch (e) {
       console.error("Something went wrong");
     }
@@ -90,7 +95,40 @@ const DoneModal = () => {
     >
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
-          <Text>dfoihfdfjdg oghdoifdh ffhdfd fdfdf fdofgdhj</Text>
+          {serviceWithCharges?.map((service, i) => (
+            <View key={i} style={styles.service_Name_And_Charges_Container}>
+              <Text style={styles.textServiceNameAndCharges}>
+                {service.name}
+              </Text>
+              <Text style={styles.textServiceNameAndCharges}>
+                {service.charges + " Rs"}
+              </Text>
+            </View>
+          ))}
+
+          <View style={styles.service_Name_And_Charges_Container}>
+            <Text style={styles.textServiceNameAndCharges}>
+              extra charges(optional)
+            </Text>
+            <Input
+              value={extra}
+              onChangeText={(text) => {
+                let number = Number(text);
+                let boolean = isNaN(number);
+                if (!boolean) {
+                  let removedSpaces = text.replace(/ /g, "");
+                  setExtra(removedSpaces);
+                }
+              }}
+              style={{ width: "30%", paddingLeft: 5 }}
+              containerStyle={{ width: "30%" }}
+            />
+          </View>
+
+          <View style={styles.totalWithCharges}>
+            <Text style={styles.bolder}>total</Text>
+            <Text style={styles.bolder}>{customerPaid} Rs</Text>
+          </View>
           <View style={styles.buttonsContainer}>
             <Button
               onPress={() => {
@@ -109,7 +147,6 @@ const DoneModal = () => {
             <Button
               buttonStyle={{
                 backgroundColor: "wheat",
-
                 margin: 3,
                 paddingVertical: 0,
                 paddingHorizontal: 3,
@@ -156,5 +193,29 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     width: "100%",
+  },
+  service_Name_And_Charges_Container: {
+    flexDirection: "row",
+    width: "100%",
+    // borderWidth: 1,
+    // borderColor: "red",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  textServiceNameAndCharges: {
+    fontWeight: "bold",
+  },
+  totalWithCharges: {
+    flexDirection: "row",
+    width: "95%",
+    justifyContent: "space-between",
+    padding: 10,
+    borderTopColor: "black",
+    borderTopWidth: 2,
+  },
+  bolder: {
+    fontWeight: "bold",
+    fontSize: 17,
   },
 });
--- screens/Report/EachReport.js
+++ screens/Report/EachReport.js
@@ -18,9 +18,20 @@ const EachReport = ({ item }) => {
         </View>
       ))}
 
+      {item.extra !== 0 && item.extra !== undefined ? (
+        <View style={styles.service_Name_And_Charges_Container}>
+          <Text style={styles.textServiceNameAndCharges}>extra charges</Text>
+          <Text style={styles.textServiceNameAndCharges}>
+            {item.extra + " Rs"}
+          </Text>
+        </View>
+      ) : null}
+
       <View style={styles.totalWithCharges}>
         <Text style={styles.bolder}>total</Text>
-        <Text style={styles.bolder}>{item.customerPaid} Rs</Text>
+        <Text style={styles.bolder}>
+          {Number(item.customerPaid) + item.extra} Rs
+        </Text>
       </View>
 
       <View style={styles.Provider_Date_Time_container}>
