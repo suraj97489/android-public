b08ea89e23978e9cae8743f5d9f08a7351981bc8
Suraj Atigre <suraj97489@gmail.com>
2022-02-08 14:00:02 +0530

    added useEffect updateSalonProvidersfordisplay

--- components/SpServiceproviderslist.js
+++ components/SpServiceproviderslist.js
@@ -1,9 +1,21 @@
-import react from "react";
+import react, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import ProviderAfter from "./ProviderAfter";
 import ProviderBefore from "./ProviderBefore";
+import { useDispatch, useSelector } from "react-redux";
+import { updateSalonProvidersfordisplay } from "../features/salon/salonSlice";
 
 const SpServiceproviderslist = ({ provider, index }) => {
+  const salon = useSelector((state) => state.salon.salon);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const payLoad = salon?.serviceproviders.map((provider) => ({
+      ...provider,
+      display: "none",
+    }));
+    dispatch(updateSalonProvidersfordisplay(payLoad));
+  }, []);
   return (
     <View style={styles.providerAll}>
       <ProviderBefore provider={provider} />
