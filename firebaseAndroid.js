6db52ec49d95e1f2ca147bd8414ba02b9617e12f
Suraj Atigre <suraj97489@gmail.com>
2022-02-10 15:28:14 +0530

    dotenv added and updated firebase config

--- firebaseAndroid.js
+++ firebaseAndroid.js
@@ -3,22 +3,31 @@ import { getAuth } from "firebase/auth";
 
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
 
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_API_KEY,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_APP_ID,
-  // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  apiKey: "AIzaSyAUv7Pq-QMdrGKXR7UCzytHbYw2AHxHkkk",
-  authDomain: "salonkattanew.firebaseapp.com",
-  projectId: "salonkattanew",
-  storageBucket: "salonkattanew.appspot.com",
-  messagingSenderId: "786462931290",
-  appId: "1:786462931290:web:e29160555434d424ec5b63",
-  measurementId: "G-MRCG06HR18",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+  // apiKey: "AIzaSyAUv7Pq-QMdrGKXR7UCzytHbYw2AHxHkkk",
+  // authDomain: "salonkattanew.firebaseapp.com",
+  // projectId: "salonkattanew",
+  // storageBucket: "salonkattanew.appspot.com",
+  // messagingSenderId: "786462931290",
+  // appId: "1:786462931290:web:e29160555434d424ec5b63",
+  // measurementId: "G-MRCG06HR18",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
