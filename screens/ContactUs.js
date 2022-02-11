cb0458ea695b469ce7a18755409f5881194bbb02
Suraj Atigre <suraj97489@gmail.com>
2022-02-11 09:55:14 +0530

    updated contactUs.js

--- screens/ContactUs.js
+++ screens/ContactUs.js
@@ -3,23 +3,25 @@ import { StyleSheet, Text, View } from "react-native";
 import { Button, Input } from "react-native-elements";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebaseAndroid";
+import { useSelector } from "react-redux";
 
 const ContactUs = () => {
+  const salon = useSelector((state) => state.salon.salon);
   let initialState = {
     fname: "",
     lname: "",
-    email: "",
+    salonCode: salon.salonCode,
+    salonName: salon.salonName,
     message: "",
   };
   const [formData, setFormData] = useState(initialState);
 
   function submitContactUs() {
-    addDoc(collection(db, "salon"), formData);
+    addDoc(collection(db, "contactUs"), formData);
   }
   return (
     <>
       <View style={styles.contact_us}>
-        <Text style={styles.heading}>Contact Us</Text>
         <View style={styles.topContainer}>
           <Text
             style={{
@@ -29,8 +31,9 @@ const ContactUs = () => {
           >
             1472/21,D WARD ,SHUKRAWAR PETH ,MASKUTI TALAV,KOLHAPUR.
           </Text>
+          <Text style={{ color: "white" }}>7020675593</Text>
           <Text style={{ color: "white" }}>8484845040</Text>
-          <Text style={{ color: "white" }}>info@salonkatta.com</Text>
+          {/* <Text style={{ color: "white" }}>info@salonkatta.com</Text> */}
         </View>
         <Text style={styles.question}>How Can We Help You?</Text>
         <View style={styles.form}>
@@ -50,14 +53,7 @@ const ContactUs = () => {
               setFormData({ ...formData, lname: text });
             }}
           />
-          <Text>Email</Text>
-          <Input
-            value={formData.email}
-            placeholder="email..."
-            onChangeText={(text) => {
-              setFormData({ ...formData, email: text });
-            }}
-          />
+
           <Text>Message</Text>
           <Input
             value={formData.message}
@@ -68,6 +64,11 @@ const ContactUs = () => {
           />
           <Button
             onPress={submitContactUs}
+            disabled={
+              formData.fname.length < 2 ||
+              formData.lname.length < 2 ||
+              formData.message.length < 30
+            }
             containerStyle={{ width: 150, margin: 20 }}
             title="send"
           />
